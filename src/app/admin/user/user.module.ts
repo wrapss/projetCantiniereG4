7c31e028d57f032a471b789b06e917de9a3f21ea
Jerome_Kraft <kraft.jerome@yahoo.com>
2023-01-08 17:36:39 +0100

    Update user.module.ts

--- src/app/admin/user/user.module.ts
+++ src/app/admin/user/user.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+import { MatTableModule } from "@angular/material/table";
+import { MatIconModule } from "@angular/material/icon";
 import { UserRoutingModule } from './user-routing.module';
 import { UListComponent } from './u-list/u-list.component';
 import { UEditComponent } from './u-edit/u-edit.component';
-import { MatTableModule } from "@angular/material/table";
-import { MatIconModule } from "@angular/material/icon";
-import { FormsModule } from "@angular/forms";
 
 /* Renvoyés dans le général "app.module.ts" pour s'appliquer dans tous les modules de l'app
 import { LOCALE_ID } from '@angular/core';
