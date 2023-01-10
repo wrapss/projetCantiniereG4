7350205c42071cd4df81fcab54fdc6db3ec3abe4
Jerome_Kraft <kraft.jerome@yahoo.com>
2023-01-10 01:41:13 +0100

    Modifications sur Public/Carte/Carte-component
    * Mise en forme des fichiers (suppressions des espaces supplémentaires, cohésion graphique, typage des données et retours)
    * Ajout d'un entête de travail pour identifier l'élément (A SUPPRIMER par la suite)
    * Ajout du pipe " | currency: 'EUR' " dans le .html

--- src/app/public/carte/carte.component.ts
+++ src/app/public/carte/carte.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {MealService} from "../../_services/meal.service";
-import {IMenu} from "../../_interfaces/menu";
-import {IMeal} from "../../_interfaces/meal";
+import { MealService } from "../../_services/meal.service";
+import { IMeal } from "../../_interfaces/meal";
+import { Category } from '../../_interfaces/category';
 
 @Component({
   selector: 'app-carte',
@@ -10,43 +10,42 @@ import {IMeal} from "../../_interfaces/meal";
 })
 export class CarteComponent implements OnInit {
 
-  Categories = [
+  public categories: Category[] =[
     {id: 1, name: 'appetizers'},
     {id: 2, name: 'starters'},
     {id: 3, name: 'appetizers'},
     {id: 5, name: 'others'},
     {id: 7, name: 'brunchs_and_lunches'},
     {id: 9, name: 'sauces'},
-
   ];
+  private _allMeals: IMeal[] = [];
 
-  AllMenus: IMeal[] = [
-  ]
-
-  constructor(private mealService: MealService) { }
+  constructor(private _mealService: MealService) { }
 
   ngOnInit(): void {
-    console.log(this.getImageMealByID())
-    this.mealService.getAllMeals().subscribe(
+    console.log(this.getImageMealByID());   // TODO : rajouter les icones de menus
+    this.getAllMeal();
+  }
+
+  private getImageMealByID() {
+    return this._mealService.getImageMenuByID(1).subscribe(
+      data => {
+        console.log(data);
         // @ts-ignore
-        data => this.AllMenus = data
-    )
-
-  }
-
-  getMealByCategory(category: any){
-    return this.AllMenus.filter((f) => f.category == category)
-  }
-
-  getImageMealByID(){
-     return this.mealService.getImageMenuByID(1).subscribe(
-        data => {
-          console.log(data)
-          // @ts-ignore
-          return 'http://localhost:8080/stone.lunchtime/'+data.imagePath
-        }
+        return 'http://localhost:8080/stone.lunchtime/'+ data.imagePath;
+      }
     )
   }
-  //
+
+  private getAllMeal(): void {
+    this._mealService.getAllMeals().subscribe(
+      // @ts-ignore
+      data => this._allMeals = data
+    );
+  }
+
+  public getMealByCategory(category: any): IMeal[] {
+    return this._allMeals.filter((f) => f.category == category);
+  }
 
 }
